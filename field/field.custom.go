fa0c2f8ccc1aef04684a1a5228c9b010237dbbc1
ShuQingZai <overbeck.jack@outlook.com>
2026-03-10 12:59:19 +0800

    feat(field): 添加字段比较和算术运算方法
    - 实现了大于、小于、等于等比较操作方法(Gt, Lt, Gte, Lte)
    - 添加了范围查询支持(Between方法)
    - 实现了IN和NOT IN查询功能
    - 添加了基本算术运算方法(Incr, Decr, Mul, Div)
    - 支持表达式间的比较操作(GtExpr, LtExpr等)
    - 提供了类型安全的值转换机制

---
+++ field/field.custom.go
@@ -0,0 +1,93 @@
+package field
+
+import (
+	"gorm.io/gorm/clause"
+)
+
+// Gt creates a greater than comparison expression (field > value).
+func (f Field[T]) Gt(value T) clause.Expression {
+	return clause.Gt{Column: f.column, Value: value}
+}
+
+// GtExpr creates a greater than comparison expression (field > expression).
+func (f Field[T]) GtExpr(expr clause.Expression) clause.Expression {
+	return clause.Gt{Column: f.column, Value: expr}
+}
+
+// Gte creates a greater than or equal comparison expression (field >= value).
+func (f Field[T]) Gte(value T) clause.Expression {
+	return clause.Gte{Column: f.column, Value: value}
+}
+
+// GteExpr creates a greater than or equal comparison expression (field >= expression).
+func (f Field[T]) GteExpr(expr clause.Expression) clause.Expression {
+	return clause.Gte{Column: f.column, Value: expr}
+}
+
+// Lt creates a less than comparison expression (field < value).
+func (f Field[T]) Lt(value T) clause.Expression {
+	return clause.Lt{Column: f.column, Value: value}
+}
+
+// LtExpr creates a less than comparison expression (field < expression).
+func (f Field[T]) LtExpr(expr clause.Expression) clause.Expression {
+	return clause.Lt{Column: f.column, Value: expr}
+}
+
+// Lte creates a less than or equal comparison expression (field <= value).
+func (f Field[T]) Lte(value T) clause.Expression {
+	return clause.Lte{Column: f.column, Value: value}
+}
+
+// LteExpr creates a less than or equal comparison expression (field <= expression).
+func (f Field[T]) LteExpr(expr clause.Expression) clause.Expression {
+	return clause.Lte{Column: f.column, Value: expr}
+}
+
+// Between creates a range comparison expression (field BETWEEN v1 AND v2).
+func (f Field[T]) Between(v1, v2 T) clause.Expression {
+	return clause.And(
+		clause.Gte{Column: f.column, Value: v1},
+		clause.Lte{Column: f.column, Value: v2},
+	)
+}
+
+// In creates an IN comparison expression (field IN (values...)).
+func (f Field[T]) In(values ...T) clause.Expression {
+	interfaceValues := make([]any, len(values))
+	for i, v := range values {
+		interfaceValues[i] = v
+	}
+	return clause.IN{Column: f.column, Values: interfaceValues}
+}
+
+// NotIn creates a NOT IN comparison expression (field NOT IN (values...)).
+func (f Field[T]) NotIn(values ...T) clause.Expression {
+	interfaceValues := make([]any, len(values))
+	for i, v := range values {
+		interfaceValues[i] = v
+	}
+	return clause.Not(clause.IN{Column: f.column, Values: interfaceValues})
+}
+
+// Basic SQL expression functions for arithmetic operations
+
+// Incr creates an increment expression (field + value).
+func (f Field[T]) Incr(value T) AssignerExpression {
+	return colOpExpr{col: f.column, sql: "? + ?", vars: []any{f.column, value}}
+}
+
+// Decr creates a decrement expression (field - value).
+func (f Field[T]) Decr(value T) AssignerExpression {
+	return colOpExpr{col: f.column, sql: "? - ?", vars: []any{f.column, value}}
+}
+
+// Mul creates a multiplication expression (field * value).
+func (f Field[T]) Mul(value T) AssignerExpression {
+	return colOpExpr{col: f.column, sql: "? * ?", vars: []any{f.column, value}}
+}
+
+// Div creates a division expression (field / value).
+func (f Field[T]) Div(value T) AssignerExpression {
+	return colOpExpr{col: f.column, sql: "? / ?", vars: []any{f.column, value}}
+}
